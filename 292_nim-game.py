a9c632909bf461550d332022d4ceb33235a86692
xianglei <657984027@qq.com>
2018-11-06 15:50:02 +0800

    292

---
+++ 292_nim-game.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n % 4 != 0
+
+print Solution().canWinNim(5)
