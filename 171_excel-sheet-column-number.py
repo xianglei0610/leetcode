b76d69ab1fe64d9a8acdfdff394231e98249e49c
xianglei <657984027@qq.com>
2018-11-07 18:13:50 +0800

    171

---
+++ 171_excel-sheet-column-number.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = 0
+        for i in s:
+            result = result*26 + ord(i) - ord('A')+1
+        return result
+print Solution().titleToNumber('ZY')
