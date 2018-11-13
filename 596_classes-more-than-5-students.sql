c6c607f8f5dcaf625eba653a5db2e7608dbd3688
xianglei <657984027@qq.com>
2018-11-13 17:16:45 +0800

    596

--- 596_classes-more-than-5-students.sql
+++ 596_classes-more-than-5-students.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select A.class from  (select  distinct student,class  from courses ) as A group by A.class having   count(A.student)>5;
+select A.class from  (select  distinct student,class  from courses ) as A group by A.class having   count(A.student)>=5;
