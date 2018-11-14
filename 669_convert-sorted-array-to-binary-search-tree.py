4660caf74681bb0e59a2e857e251a652e8bafb97
xianglei <657984027@qq.com>
2018-11-14 11:30:05 +0800

    669

---
+++ 669_convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: xianglei
+@time: 2018/11/14 11:26
+@desc:
+'''
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root.val>R:
+            return self.trimBST(root.left,L,R)
+        if root.val<L:
+            return self.trimBST(root.right, L, R)
+        if root.val>=L and root.val<=R:
+            root.left = self.trimBST(root.left,L,R)
+            root.right = self.trimBST(root.right, L, R)
+            return root
+
