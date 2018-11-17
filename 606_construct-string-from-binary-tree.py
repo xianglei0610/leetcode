a53814e833eceb4be304a9ef0a6c4f21d9fcdfc1
xianglei <657984027@qq.com>
2018-11-17 16:04:53 +0800

    606

---
+++ 606_construct-string-from-binary-tree.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+from test._mock_backport import right
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def tree2str(self, t):
+        """
+        :type t: TreeNode
+        :rtype: str
+        """
+        if not t:
+            return ''
+        res = ''
+        left = self.tree2str(t.left)
+        right = self.tree2str(t.right)
+        if left or right:
+            res += '(%s)' % left
+        if right:
+            res +='(%s)' % right
+        return str(t.val) + res
+            
