61a8f916d7cbfef7b3223f7f49dd5015e5b8def8
xianglei <657984027@qq.com>
2018-11-18 17:14:41 +0800

    563

---
+++ 563_binary-tree-tilt.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    
+    
+    def findTilt(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+        self.ans = 0
+        def getSum(root):
+            if not root:
+                return 0
+            left = getSum(root.left)
+            right = getSum(root.right)
+            self.ans +=  ans(left-right)
+            return root.val+left+right
+        getSum(root)
+        return self.ans
+        
