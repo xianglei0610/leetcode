83b700b27e9c341651c0f2ec488729cd9448f6b6
xianglei <657984027@qq.com>
2018-11-20 14:20:23 +0800

    671

--- 671_second-minimum-node-in-a-binary-tree.py
+++ 671_second-minimum-node-in-a-binary-tree.py
@@ -14,3 +14,19 @@ class Solution(object):
         :rtype: int
         """
         
+        if not root:
+            return -1
+        s =set()
+        que = [root]
+        while que:
+            m = que.pop()
+            s.add(m.val)
+            if m.left:
+                que.append(m.left)
+            if m.right:
+                que.append(m.right)
+        if len(s)<2:
+            return -1
+        else:
+            s.remove(min(s))
+            return min(s)
