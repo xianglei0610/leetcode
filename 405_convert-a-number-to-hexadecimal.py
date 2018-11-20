4316b52541027e758fb901e3c7d76055c948a3fd
xianglei <657984027@qq.com>
2018-11-20 15:04:52 +0800

    405

---
+++ 405_convert-a-number-to-hexadecimal.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+
+class Solution(object):
+    def toHex(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        dict={10:'a',11:'b',12:'c',13:'d',14:'e',15:'f'}
+        hexRes=[]
+        if num<0:
+            num+=2**32#处理负数的方法
+        if num==0:
+            return '0'
+        
+        while num>0:
+            f = num%16
+            num /= 16
+            if f>=0 and f<=9:
+                hexRes.append(str(f))
+            else:
+                hexRes.append(dict(f))
+        hexRes = hexRes[::-1]
+        return ''.join(hexRes)
